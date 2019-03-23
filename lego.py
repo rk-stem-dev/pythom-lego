cf8c2fd984babc6ebeabd356b49a6986f37bb9d9
rk-stem-dev <48621083+rk-stem-dev@users.noreply.github.com>
2019-03-23 12:59:15 +0200

    Update lego.py

--- lego.py
+++ lego.py
@@ -2,6 +2,6 @@ from ev3dev2.led import Leds
 import time
 leds = Leds()
 while True:
-leds.set_color("RIGHT","RED")
-time.sleep(0.5)
-leds.set_color("LEFT","AMBER")
+  leds.set_color("RIGHT","RED")
+  time.sleep(0.5)
+  leds.set_color("LEFT","AMBER")
